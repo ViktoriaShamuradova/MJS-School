baf18a4b3e6c1630e870eb1db66c514b1f3e7c84
Viktoria Shamuradova <viktoriashamuradova@gmail.com>
2021-03-25 21:26:53 +0300

    sec conf

--- module4/web/src/main/java/com/epam/esm/configuration/SecurityConfiguration.java
+++ module4/web/src/main/java/com/epam/esm/configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -32,11 +33,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/registration").permitAll()
+                .antMatchers(HttpMethod.GET, "/registration").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                //.httpBasic();
-                .formLogin()
+                .httpBasic()
+                //.formLogin()
                 .and()
                 .logout()
                 .invalidateHttpSession(true)
